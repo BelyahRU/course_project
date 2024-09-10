c8d1e6ae4fcf582c0316ed92333f026d6ba2ab4f
Alexander <oBelyash@yandex.ru>
2024-09-11 01:12:52 +0300

    Initial Commit

---
+++ course_project/course_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  course_projectApp.swift
+//  course_project
+//
+//  Created by Александр Андреев on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct course_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
